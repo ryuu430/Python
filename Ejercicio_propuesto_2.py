be69afafe449efa22d5065021ab1db68eaa493d1
ryuu430 <ryuutempest149@gmail.com>
2025-12-05 17:12:03 +0100

    Los 3 ejercicios de python

---
+++ Ejercicio_propuesto_2.py
@@ -0,0 +1,8 @@
+fra="Hola soy Santiago"
+pa=fra.split()
+print("''' bash")
+print(f"Introduce una frase: {fra}")
+for i in pa:
+    print(i)
+print(f"Numero de palabras de la frase: {len(pa)}")
+print("'''")
