f7878799fef1ca41786fcbf2a8d8dcfe5716b732
Ryuu <ryuutempest149@gmail.com>
2025-12-05 19:53:46 +0100

    Delete Ejercicio_propuesto_2.py

--- Ejercicio_propuesto_2.py
+++
@@ -1,8 +0,0 @@
-fra="Hola soy Santiago"
-pa=fra.split()
-print("''' bash")
-print(f"Introduce una frase: {fra}")
-for i in pa:
-    print(i)
-print(f"Numero de palabras de la frase: {len(pa)}")
-print("'''")
