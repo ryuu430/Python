06fa5f827aa0e6629e81075e1660ba8852e45140
Ryuu <ryuutempest149@gmail.com>
2026-01-25 16:56:16 +0100

    Add files via upload

---
+++ U4_proyecto1_(parte_3)/Ejercicio_propuesto_10.py
@@ -0,0 +1,8 @@
+# tupla desordenada
+tup = ('C', 'h', 'R', 'm', 'A', 's', 'M', 'o', 'T', 'y', 'c')
+lis= list(tup)
+lis.sort()
+
+tup= tuple(lis)
+# tupla ordenada
+print(tup) # ('A', 'C', 'M', 'R', 'T', 'c', 'h', 'm', 'o', 's', 'y')
---
+++ U4_proyecto1_(parte_3)/Ejercicio_propuesto_11.py
@@ -0,0 +1,6 @@
+tup = (3,4,2,5,5,5,2,3)
+lis= list(tup)
+lis.sort()
+tup= tuple(lis)
+# lo mismo que la act. anterior pero con numeros
+print(tup)# (2, 2, 3, 3, 4, 5, 5, 5)
