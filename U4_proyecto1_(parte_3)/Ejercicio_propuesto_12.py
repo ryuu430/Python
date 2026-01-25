e97cfb5a7f0d2bd62db558e5ca30f8a5056f2d48
Ryuu <ryuutempest149@gmail.com>
2026-01-25 17:13:40 +0100

    Add files via upload

---
+++ U4_proyecto1_(parte_3)/Ejercicio_propuesto_12.py
@@ -0,0 +1,4 @@
+# una tupla en otra tupla
+anim = (("gato",len("gato") ),("perro",len("perro") ),("canario",len("canario") ),("pescado",len("pescado") ),("conejo",len("conejo") ),("hámster",len("hamster")))
+
+print(anim)
